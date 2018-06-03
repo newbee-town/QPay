78e8806e050e4994283545271df489e71a22dec6
newbee-town <newbee_town@outlook.com>
2018-06-03 16:05:43 +0800

    init

---
+++ QPay.ts
@@ -0,0 +1,29 @@
+class Qpay{
+    
+    constructor() {
+        // define fields stored to state trie.
+        LocalContractStorage.defineMapProperties(this, {
+            height: null,
+            width: null,
+        });
+    }
+
+    // init function.
+    init(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    // calc area function.
+    calcArea() {
+        return this.height * this.width;
+    }
+
+    // verify function.
+    verify(expected) {
+        let area = this.calcArea();
+        if (expected != area) {
+            throw new Error("Error: expected " + expected + ", actual is " + area + ".");
+        }
+    }
+}
